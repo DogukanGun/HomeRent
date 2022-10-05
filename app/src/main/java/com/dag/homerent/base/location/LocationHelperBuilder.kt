a6092e4ecd33dec78ca487d634c2117e72676f69
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2022-10-05 22:22:48 +0300

    Location Helper was created for hms and gms

---
+++ app/src/main/java/com/dag/homerent/base/location/LocationHelperBuilder.kt
@@ -0,0 +1,19 @@
+package com.dag.homerent.base.location
+
+import android.content.Context
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+
+class LocationHelperBuilder constructor(
+    var context: Context
+) {
+    fun getLocationHelper(): ILocationHelper {
+        val code = GoogleApiAvailability.getInstance()
+            .isGooglePlayServicesAvailable(context.applicationContext)
+        return if (code != ConnectionResult.SUCCESS) {
+            LocationHelperGms(context)
+        } else {
+            LocationHelperHms(context)
+        }
+    }
+}
